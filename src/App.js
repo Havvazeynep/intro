b94743fcb671e8d7d0251fba4b5163897f3caa0e
Havva Zeynep Akdemir <83979496+Havvazeynep@users.noreply.github.com>
2022-10-04 14:59:41 +0300

    Routes oluşturuldu, CartList işlemleri yapıldı

--- src/App.js
+++ src/App.js
@@ -4,50 +4,53 @@ import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 import alertify from "alertifyjs";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./NotFound";
+import CartList from "./CartList";
 
 export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] };
 
-  state={currentCategory: "", products:[], cart:[] };
-
-  componentDidMount(){
+  componentDidMount() {
     this.getProducts();
   }
 
   changeCategory = (category) => {
-    this.setState({currentCategory:category.categoryName});
+    this.setState({ currentCategory: category.categoryName });
     this.getProducts(category.id);
   };
 
-  getProducts = categoryId => {
+  getProducts = (categoryId) => {
     let url = "http://localhost:3000/products";
-    if(categoryId) {
+    if (categoryId) {
       url += "?categoryId=" + categoryId;
     }
     fetch(url)
-    .then(response=>response.json())
-    .then(data=>this.setState({products:data}));
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
   };
 
-  addToCart = (product)=>{
+  addToCart = (product) => {
     let newCart = this.state.cart;
-    var addedItem = newCart.find(c=>c.product.id === product.id);
-    if(addedItem){
-      addedItem.quantity+=1;
+    var addedItem = newCart.find((c) => c.product.id === product.id);
+    if (addedItem) {
+      addedItem.quantity += 1;
     } else {
-      newCart.push({product:product,quantity:1});
+      newCart.push({ product: product, quantity: 1 });
     }
-    this.setState({cart:newCart});
-    alertify.success(product.productName + "added to cart",2);
-  }
+    this.setState({ cart: newCart });
+    alertify.success(product.productName + "added to cart", 2);
+  };
 
-  removeFromCart = (product)=>{
-    let newCart = this.state.cart.filter(c=>c.product.id!==product.id);
-    this.setState({cart:newCart});
-  }
+  removeFromCart = (product) => {
+    let newCart = this.state.cart.filter((c) => c.product.id !== product.id);
+    this.setState({ cart: newCart });
+    alertify.error(product.productName + "removed from cart", 2);
+  };
 
   render() {
-    let categoryInfo = {title:"Category List"}
-    let ProductInfo = {title:"Product List"}
+    let categoryInfo = { title: "Category List" };
+    let ProductInfo = { title: "Product List" };
     return (
       <div>
         <Container>
@@ -56,10 +59,36 @@ export default class App extends Component {
           </Row>
           <Row>
             <Col xs="3">
-              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              />
             </Col>
             <Col xs="9">
-              <ProductList products={this.state.products} addToCart={this.addToCart} currentCategory={this.state.currentCategory} info={ProductInfo} />
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ProductList
+                      products={this.state.products}
+                      addToCart={this.addToCart}
+                      currentCategory={this.state.currentCategory}
+                      info={ProductInfo}
+                    />
+                  }
+                />
+                <Route
+                  path="/cart"
+                  element={
+                    <CartList
+                      cart={this.state.cart}
+                      removeFromCart={this.removeFromCart}
+                    />
+                  }
+                />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
             </Col>
           </Row>
         </Container>
--- src/cartSummary.js
+++ src/cartSummary.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import {
   UncontrolledDropdown,
   DropdownItem,
@@ -25,7 +26,9 @@ export default class extends Component {
             </DropdownItem>
           ))}
           <DropdownItem divider />
-          <DropdownItem>Reset</DropdownItem>
+          <DropdownItem>
+            <Link to="cart">Cart List</Link>
+          </DropdownItem>
         </DropdownMenu>
       </UncontrolledDropdown>
     );
